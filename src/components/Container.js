f3304653a95e3eaef13845e769977c989aa4e909
= <valigac@gmail.com>
2020-09-28 10:33:20 +0200

    Lifting State up.

--- src/components/Container.js
+++ src/components/Container.js
@@ -9,7 +9,9 @@ export default class Container extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: [ 'C0', 'E0', 'G0' ]
+      selected: [ 'C0', 'E0', 'G0' ],
+      chord: 'C major',
+      mode: 0,
     };
     this.scales = getScales();
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -20,12 +22,15 @@ export default class Container extends React.Component {
       let intervals = getInterval(input, this.scales, mode);
       this.setState({ selected: intervals });
     }
+    this.setState({ chord: input, mode: mode });
   }
 
   render() {
+    // <InputForm onInputChange={this.handleInputChange} inversions={this.state.selected.length} mode={this.state.mode} value={this.state.value} />
+    // <Piano selected={this.state.selected} />
     return(
       <div id="chord-container">
-        <InputForm onInputChange={this.handleInputChange} inversions={this.state.selected.length} />
+        <InputForm onInputChange={this.handleInputChange} inversions={this.state.selected.length} mode={this.state.mode} chord={this.state.chord} />
         <Piano selected={this.state.selected} />
       </div>
     );
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -4,27 +4,27 @@ export default class InputForm extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      value: 'C major',
-      mode: 0,
-    };
+    // this.state = {
+    //   value: 'C major',
+    //   mode: 0,
+    // };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(e) {
-    this.setState({ value: e.target.value });
-    this.props.onInputChange(e.target.value, this.state.mode);
+    // this.setState({ value: e.target.value });
+    this.props.onInputChange(e.target.value, this.props.mode);
   }
 
   handleInversion(e) {
-    this.setState({ mode: e });
-    this.props.onInputChange(this.state.value, e);
+    // this.setState({ mode: e });
+    this.props.onInputChange(this.props.chord, e);
   }
 
   componentDidUpdate() {
-    if (this.props.inversions != 4 && this.state.mode == 3) {
-      this.setState({ mode: 0});
-      this.props.onInputChange(this.state.value, 0);
+    if (this.props.inversions != 4 && this.props.mode == 3) {
+      // this.setState({ mode: 0});
+      this.props.onInputChange(this.props.chord, 0);
     }
   }
 
@@ -34,14 +34,14 @@ export default class InputForm extends React.Component {
         <div id="search-bar">
           <h2>Chord symbol</h2>
           <p>Write name of a chord (for example <span className="highlight">A minor</span>). Please separate parts of chords by space (for example "C#major7" write as "C# major 7")</p>
-          <input type="text" id="search-input" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" id="search-input" value={this.props.chord} onChange={this.handleChange} />
         </div>
 
         <div id="inversions">
-          <span className={`inversion ${(this.state.mode == 0) ? 'active-choice' : ''}`} onClick={(e) => { this.handleInversion(0) }}>Root</span>
-          <span className={`inversion ${(this.state.mode == 1) ? 'active-choice' : ''}`} onClick={(e) => { this.handleInversion(1) }}>First</span>
-          <span className={`inversion ${(this.state.mode == 2) ? 'active-choice' : ''}`} onClick={(e) => { this.handleInversion(2) }}>Second</span>
-          { this.props.inversions == 4 && <span className={`inversion ${(this.state.mode == 3) ? 'active-choice' : ''}`} onClick={(e) => { this.handleInversion(3) }}>Third</span> }
+          <span className={`inversion ${(this.props.mode == 0) ? 'active-choice' : ''}`} onClick={(e) => { this.handleInversion(0) }}>Root</span>
+          <span className={`inversion ${(this.props.mode == 1) ? 'active-choice' : ''}`} onClick={(e) => { this.handleInversion(1) }}>First</span>
+          <span className={`inversion ${(this.props.mode == 2) ? 'active-choice' : ''}`} onClick={(e) => { this.handleInversion(2) }}>Second</span>
+          { this.props.inversions == 4 && <span className={`inversion ${(this.props.mode == 3) ? 'active-choice' : ''}`} onClick={(e) => { this.handleInversion(3) }}>Third</span> }
         </div>
 
       </div>
